c6297e46597fcdcab617889e4ecb6d77df028b81
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-07-05 17:42:02 -0300

    🎉  first commit

---
+++ app/root.tsx
@@ -0,0 +1,35 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Regex Guide",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+const Root = () => {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export default Root
---
+++ app/routes/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return (<h1>Ahhh... Hello?</h1>);
+}
+
+export default Index
