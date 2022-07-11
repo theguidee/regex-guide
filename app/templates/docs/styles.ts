fe4a3f67ea97cdb738b588c4ec626917f6384bdb
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-07-11 10:22:39 -0300

    ⚡  chore: add styles to markdown content

--- app/templates/docs/styles.ts
+++ app/templates/docs/styles.ts
@@ -14,4 +14,46 @@ export const Wrapper = styled.div`
   `}
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+
+    h1 {
+      margin-bottom: ${theme.spacings.xxlarge};
+    }
+
+    h2,
+    h3 {
+      margin-top: ${theme.spacings.xlarge};
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    p {
+      margin-block: ${theme.spacings.small};
+    }
+
+    li {
+      list-style-position: inside;
+      list-style-type: initial;
+    }
+
+    code:not([class]) {
+      background: #161b22;
+      padding: 2px 5px;
+      border-radius: 3px;
+    }
+
+    blockquote {
+      margin-block: ${theme.spacings.medium};
+      font-style: italic;
+      text-align: center;
+      width: 80%;
+      margin-inline: auto;
+
+      &::before,
+      &::after {
+        content: '"';
+      }
+    }
+  `}
+`;
